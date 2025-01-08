4fbab4c10673f5df7aac09eae54de3f5eec1e8c2
Benjamin L J McCarthy <lukasmacuk@gmail.com>
2025-01-08 16:27:39 +0000

    Initial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className='text-3xl bg-slate-200 font-bold content-center h-screen'>
+        Hello World
+      </p>
+    </>
+  )
+}
+
+export default App
