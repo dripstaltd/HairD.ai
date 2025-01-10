add48c8dbb3bf9071517ef9ab0fc8b9215b9671c
Benjamin L J McCarthy <lukasmacuk@gmail.com>
2025-01-10 08:34:29 +0000

    Setup: Eslint & prettier | dev

--- client/src/components/Upload.component.jsx
+++ client/src/components/Upload.component.jsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-export default function Upload () {
+export default function Upload() {
   const [fileInputState, setFileInputState] = useState(''); // store the input value
-  const [previewSource ,setPreviewSource] = useState('');
+  const [previewSource, setPreviewSource] = useState([]); // array for multiple previews
 
   // When user uploads the file
   const handleFileInputChange = (e) => {
-    const file = e.target.files[0] // single file upload
+    const file = e.target.files[0]; // single file upload
     previewFile(file);
-  }
+  };
 
   // sets previews source
   const previewFile = (file) => {
@@ -16,49 +16,54 @@ export default function Upload () {
     reader.readAsDataURL(file);
     reader.onloadend = () => {
       setPreviewSource(reader.result);
-    }
-  }
+    };
+  };
 
   // Handles form submit -> triggers upload image function if there is an image
   const handleSubmitFile = (e) => {
     e.preventDefault();
-    if(!previewSource) return
+    if (!previewSource) return;
     uploadImage(previewSource);
-  }
+  };
 
   // Uploads file to the api
   const uploadImage = async (base64Image) => {
     try {
       await fetch('/api/upload', {
         method: 'POST',
-        body: JSON.stringify({data: base64Image}),
-        headers: {'Content-type': 'application/json'}
+        body: JSON.stringify({ data: base64Image }),
+        headers: { 'Content-type': 'application/json' },
       });
     } catch (err) {
       console.error(err);
     }
-  }
+  };
 
   return (
-    <div className='flex flex-col w-10/12 m-auto'>
-      <h1 className='font-bold text-5xl p-4'>Upload</h1>
-      <form className='flex flex-col' onSubmit={handleSubmitFile}>
+    <div className="flex flex-col w-10/12 m-auto">
+      <h1 className="font-bold text-5xl p-4">Upload</h1>
+      <form className="flex flex-col" onSubmit={handleSubmitFile}>
         <input
-          type='file'
-          name='image'
-          className='form-input p-4'
+          type="file"
+          name="image"
+          className="form-input p-4"
           onChange={handleFileInputChange}
           value={fileInputState}
         />
-        <button className='px-6 py-3.5 text-base font-medium text-white bg-purple-400 hover:bg-purple-500 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center' type='submit'>Submit</button>
+        <button
+          className="px-6 py-3.5 text-base font-medium text-white bg-purple-400 hover:bg-purple-500 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center"
+          type="submit"
+        >
+          Submit
+        </button>
       </form>
       {previewSource && (
         <img
           src={previewSource}
-          alt='uploaded image'
-          className='w-60 rounded-lg my-6'
+          alt="uploaded image"
+          className="w-60 rounded-lg my-6"
         />
       )}
     </div>
-  )
+  );
 }
