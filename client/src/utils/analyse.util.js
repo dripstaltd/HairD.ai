edc9a7d1528bc296657560c875b7f7dec50942bc
Benjamin L J McCarthy <lukasmacuk@gmail.com>
2025-01-10 11:25:33 +0000

    Added: util to calculate the mean shade

---
+++ client/src/utils/analyse.util.js
@@ -0,0 +1,11 @@
+export default function analyse(hairDataObject) {
+  const shadeData = hairDataObject.map((hairData) => hairData.base_shade);
+
+  // Sum of all numbers in the array
+  const sum = shadeData.reduce((acc, num) => acc + num, 0);
+
+  // calc the mean
+  const mean = sum / shadeData.length;
+
+  return mean;
+}
