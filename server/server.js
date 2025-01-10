537c755a658151e7586ef043a6e3672491be9a7f
Benjamin L J McCarthy <lukasmacuk@gmail.com>
2025-01-10 09:37:40 +0000

    Fix: increased size of image crop | cloudinary

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import 'dotenv/config'
+import 'dotenv/config';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import cloudinary from './utils/cloudinary.js';
@@ -27,26 +27,34 @@ app.post('/api/upload', async (req, res) => {
       throw new Error('Image data is missing');
     }
 
-    const uploadResult = await cloudinary.uploader.upload(base64EncodedImageString, {
-      upload_preset: 'hairdai',
-      transformation: [
-          { width: 100, height: 100, crop: 'crop', gravity: 'center' },
-          { effect: 'grayscale' },
-          { effect: 'blur:10' },
+    const uploadResult = await cloudinary.uploader.upload(
+      base64EncodedImageString,
+      {
+        upload_preset: 'hairdai',
+        transformation: [
+          { width: 500, height: 500, crop: 'crop', gravity: 'center' }, // Crop to center
+          { effect: 'grayscale' }, // Turn to black and white
+          { effect: 'blur:10' }, // Blur
         ],
-      // public_id: 'shoes' // create unique id
-    });
+        // public_id: 'shoes' // create unique id
+      }
+    );
 
+    // Fetch the processed image from Cloudinary and calculate the average shade
     const averageShade = await calculateAverageShade(uploadResult.secure_url);
-    console.log(averageShade);
-    res.status(200).json({msg: 'Successfully uploaded to cloudinary'});
+    console.log(averageShade); // -> number
+
+    res.status(200).json({
+      message: 'Successfully uploaded to cloudinary',
+      base_shade: averageShade,
+    });
   } catch (err) {
-    console.error(err.message)
-    res.status(500).json({error: 'Failed to upload to cloudinary 💩'});
+    console.error('Error processing image:', err.message);
+    res.status(500).json({ error: 'Failed to upload to cloudinary 💩' });
   }
 });
 
 // Listening to server
-app.listen(PORT || 3001, ()=> {
-  console.log(`Listening on port ${PORT}`)
+app.listen(PORT || 3001, () => {
+  console.log(`Listening on port ${PORT}`);
 });
