aa6092fa1586c78bad1f8751339578b080667a19
Benjamin L J McCarthy <lukasmacuk@gmail.com>
2025-01-14 12:07:57 +0000

    Added: Styling for results component

--- client/src/components/Results.component.jsx
+++ client/src/components/Results.component.jsx
@@ -9,12 +9,18 @@ export default function Results(data) {
   };
 
   const handleResults = () => {
-    if (!data) return;
-
-    results.base = data;
     return (
       <>
-        <p>{data}</p>
+        <p>Hair being Natural: True</p>
+        <p>Base hair: </p>
+        <p>Base hair undertones: </p>
+        <p>Target level: </p>
+        <p>Levels of lift: </p>
+        <p>Developer require: %</p>
+        <br></br>
+        <p className="text-gray-400 italic">
+          Based on your hair not having previously been dyed.
+        </p>
       </>
     );
   };
@@ -24,12 +30,10 @@ export default function Results(data) {
       <div className="flex flex-row justify-between p-4">
         <div>
           <p className="text-2xl flex text-white">Results:</p>
-          <div className="inset__dark w-auto">
-            <p>{handleResults}</p>
-          </div>
+          <div className="text-left text-white p-4">{handleResults()}</div>
         </div>
         {/* Side bar */}
-        <div className="card__dark flex flex-col divide-y divide-gray-800">
+        <div className="card__dark flex flex-col divide-y divide-gray-800 h-fit cursor-pointer">
           <div className="p-3">export</div>
           <div className="p-3">save</div>
           <div className="p-3">share</div>
